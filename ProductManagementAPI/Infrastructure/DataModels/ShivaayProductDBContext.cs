ac490e5da0e9c1ac4383bfee8b17ec4d8598f056
Pushpa Nagar <pushpanagar.mca@gmail.com>
2022-06-06 13:00:18 +0530

    project changes

--- ProductManagementAPI/Infrastructure/DataModels/ShivaayProductDBContext.cs
+++ ProductManagementAPI/Infrastructure/DataModels/ShivaayProductDBContext.cs
@@ -24,7 +24,7 @@ namespace ProductManagementAPI.Infrastructure.DataModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=172.29.19.137;Database=ShivaayProductDB;TrustServerCertificate=True;user id = Sofyx; password = 4zMD78xl;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=ShivaayProductDB;TrustServerCertificate=True;user id = user; password = pass;");
             }
         }
 
